c8af12fcaecd74c3438aa4cd16decd7a668c468d
diegonz2 <46681214+diegonz2@users.noreply.github.com>
2025-10-26 11:50:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
@@ -16,8 +15,6 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
